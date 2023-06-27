54aceda71cb3d32b6f4a6acda1964d72431f0bc0
MayurPondkule <mayurpondkule001@gmai.com>
2023-06-27 18:19:14 +0530

    commit

---
+++ Main.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+class Main{ 
+
+     public static void printBoard(char[][] board){
+        
+         for(int row=0;row<board.length;row++){
+            for(int col=0;col<board[row].length;col++){
+                System.out.print(board[row][col]+" | ");;
+            }
+            System.out.println();
+        }
+    }
+    
+
+    public static boolean won(char[][] board,char player){
+
+
+         for(int row=0;row<board.length;row++){
+
+            if(board[row][0]==player && board[row][1]==player && board[row][2]==player){
+                return true;
+            }
+
+         }
+
+         for(int col=0;col<board[0].length;col++){
+
+            if(board[0][col]==player && board[1][col]==player && board[2][col]==player){
+                return true;
+            }
+
+         }
+
+         if(board[0][0]==player && board[1][1]==player && board[2][2]==player){
+                return true;
+            }
+
+            if(board[0][2]==player && board[1][1]==player && board[2][0]==player){
+                return true;
+            }
+
+        return false;
+        
+    }
+    public static void main(String[] args) {
+
+        char[][] board =new char[3][3];
+
+        for(int row=0;row<board.length;row++){
+            for(int col=0;col<board[row].length;col++){
+                board[row][col]=' ';
+            }
+        }
+
+        char player ='X';
+        boolean gameOver=false;
+        Scanner sc=new Scanner(System.in);
+
+
+        while(!gameOver){
+            printBoard(board);
+            System.out.print("player "+player+" enter:");
+            int row=sc.nextInt();
+            int col=sc.nextInt();
+
+            if(board[row][col]==' '){
+                board[row][col]=player; 
+                gameOver=won( board, player);
+                if(gameOver){
+                    System.out.println("Player "+ player+" has won🎆");
+                }
+                else{
+                    player = (player =='X') ? 'O' : 'X';
+                }
+            }
+            else{
+                System.out.println("already filled");
+                 
+            }
+        }
+
+        printBoard(board);
+   
+    }
+
+   
+}
